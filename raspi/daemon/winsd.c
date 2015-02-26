7117ef1d79636301622da8d09e072b72c248f475
Grp7 <477grp7@purdue.edu>
2015-02-26 02:18:10 +0000

    rasp

--- raspi/daemon/winsd.c
+++ raspi/daemon/winsd.c
@@ -8,6 +8,8 @@
  */
 
 #define WINSD_VER "winsd_1.0"
+#define NBYTES 6
+#define NSECONDS 3
 
 #include <bcm2835.h>
 #include <stdio.h>
@@ -19,7 +21,8 @@ enum opcode {
     IMU = 'M',
     KEYPAD = 'K',
     HALT = 'H',
-    ULTRASONIC='U'
+    ULTRASONIC='U',
+    POSITION='P'
 };
 
 int main(){
@@ -48,34 +51,63 @@ int main(){
   }
 
   chdir("/");
-
   bcm2835_spi_begin();
   bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, 0);
   bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS1, 0);
-  bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_256);
+  bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_2048);
   bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
   bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
- 
-  while(1){
-    sleep(1);
-    printf( "Sending ...\n");
-    char mosi[10] = { 0x11, 0x11, 0x11, 0x11, 0x0 , 0x0, 0x0, 0x0, 0x00 };
-    char miso[10] = { 0 };
 
-    bcm2835_spi_transfernb(mosi, miso, 4);
-
-    printf("RX: ");
- 
-    int i = 0;
-    for(i = 0; i < 10; i++){
-     printf("%x " ,miso[i]);
-    }
-  
+  FILE *f = fopen("/home/pi/wins/raspi/daemon/dump.txt", "w");
+  if (f==NULL){
+     printf("Errror opening");
+     exit(1);
   }
   
- 
-  printf("\n");
+  int var = 0;
+  char mosi[NBYTES] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05};
+  char miso[NBYTES] = { 0 };
+  int j = 0;
+  for(j=0; j < NSECONDS; j++){
+  sleep(1);
+  printf("%d: going on...\n", j);
+  var = 0;
 
+  while(var<1000){
+    usleep(10000);
+
+    int i = 0;
+    for(i = 0; i<2; i++){
+      bcm2835_spi_transfer(IMU);
+    }
+    for(i = 0; i<NBYTES; i++){
+      miso[i]=bcm2835_spi_transfer(mosi[i]);
+    }
+    //bcm2835_spi_transfernb(mosi, miso, NBYTES);
+
+ 
+    fprintf(f,"var = %u, RX: ", var);
+    printf("var = %u, RX: ", var);
+    for(i = 0; i < NBYTES; i++){
+     printf("%x " ,miso[i]);
+     fprintf(f,"%x " ,miso[i]);
+    }
+    fprintf(f, " TX: ");
+    printf(" TX: ");
+    for(i = 0; i < NBYTES; i++){
+     fprintf(f,"%x " ,mosi[i]);
+     printf("%x " ,mosi[i]);
+     mosi[i]+=6;
+    }
+     fprintf(f, "\n", mosi[0]);
+     printf("\n", mosi[0]);
+    var += 1;
+  
+  }
+  }
+  printf("done\n");
+  fprintf(f,"\n");
+  fclose(f);
   bcm2835_spi_end();
   bcm2835_close();
 
