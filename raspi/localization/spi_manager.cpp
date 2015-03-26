3d455e14ad23b2930db9e032e603e6299bfe2c59
Sidharth Mudgal <sidharthmsk@gmail.com>
2015-03-25 21:54:34 -0400

    Added mising spi_manager.cpp file

---
+++ raspi/localization/spi_manager.cpp
@@ -0,0 +1,18 @@
+#include <stdexcept>
+
+#include "spi_manager.h"
+
+mutex SPI::buffer_mutex;
+string SPI::input_buffer;
+
+void SPI::Init() {
+  // TODO: Pic handshake
+}
+
+void SPI::ShowMenu() {
+  throw runtime_error("Not Implemented");
+}
+
+void SPI::ShutDown() {
+  throw runtime_error("Not Implemented");
+}
